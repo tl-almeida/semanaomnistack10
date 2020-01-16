8bd0980be64a290e11dbae8774a48cb951a19f42
tl-almeida <thiagolimadealmeid4@gmail.com>
2020-01-16 08:06:52 -0300

    frontend da aplicação com reactjs

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes')
+const cors = require('cors');
+const routes = require('./routes');
+
 
 const app = express();
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-hnacu.mongodb.net/semana10?retryWrites=true&w=majority', {
@@ -8,6 +10,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-hnacu.mongodb.net/
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
